3cb8eeaceea142740f1f161a2e188163563eec04
Vincenzo Abete <va281@cam.ac.uk>
2024-09-08 20:20:07 +0100

    Fixed issue with navigation on mobile, and improved mobile UI

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { INavigationItem } from 'interfaces/i-navigation-item';
+import { Collapse } from 'bootstrap';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
+export class NavigationComponent implements AfterViewInit{
   public items: INavigationItem[] = [
     { link: 'recipes', name: 'Recipes' },
     { link: 'settings', name: 'Settings' },
-  ]
+  ];
+  public collapse: Collapse | undefined;
+  @ViewChild('navbarNav') navbarNav: ElementRef | undefined;
+
+  public ngAfterViewInit(): void {
+    this.collapse = new Collapse(this.navbarNav?.nativeElement);
+  }
+
+  public toggleNavbarNav(): void {
+    if (this.collapse) {
+      this.collapse.toggle();
+    }
+  }
 }
